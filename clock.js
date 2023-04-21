f7521d1887ec2593f284adb1d7cfeb453246b067
jsidhu1919 <113055193+jsidhu1919@users.noreply.github.com>
2023-04-21 11:49:37 -0400

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,35 @@
+"use strict";
+
+runClock();
+ setInterval("runClock()", 1000);
+
+/* Function to create and run the countdown clock*/
+function runClock()
+{
+/*Store the current date and time*/
+var currentDay = new Date();
+var dateStr = currentDay.toLocaleDateString();
+var timeStr = currentDay.toLocaleTimeString();
+
+/*Display the current date and time*/
+document.getElementById("dateNow").innerHTML= dateStr + "<br />" + timeStr;
+
+/* Calculate the days until January 1st*/
+var newYear = new Date("May 10, 2023");
+var nextYear = currentDay.getFullYear() ;
+newYear.setFullYear(nextYear);
+var daysLeft = (newYear - currentDay)/(1000*60*60*24);
+
+/* Calculate the hours left in the current day*/
+var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+
+/*Calculate the minutes and seconds left in the current hour*/
+var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+
+/* Display the time left until New Year's Eve */
+document.getElementById("days").textContent=Math.floor(daysLeft);
+document.getElementById("hrs").textContent= Math.floor(hrsLeft);
+document.getElementById("mins").textContent=Math.floor(minsLeft);
+document.getElementById("secs").textContent=Math.floor(secsLeft);
+}
